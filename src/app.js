c68c2a183638350d755183bddb4c979f88228c86
arellak <nimble0@protonmail.com>
2022-11-02 15:17:23 +0100

    first version

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 "use strict";
 
-const { Client, Intents } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { Client, Events, Intents, Message } = require("discord.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES] });
 
 const config = require("../config.json");
+
+const userAttachments = [];
+let state;
 
 client.on("ready", async() => {
     console.log(`Logged in as ${client.user?.tag}!`);
@@ -33,6 +36,63 @@ Wenn du es dir anders überlegst, schreibe einfach "stop"!
 Bitte poste das **BANNER**:`,
         });
     }
+    state = -1;
+});
+
+async function getBanner(msg) {
+    let bannerAttachment = msg.attachments.at(0);
+
+    if(bannerAttachment.contentType === "image/png" || bannerAttachment.contentType === "image/gif") {
+        userAttachments.find(entry => entry.username === msg.author.username).attachments.banner = bannerAttachment.url;
+        return;
+    }
+    await msg.channel.send("Das war weder ein Bild noch ein Gif...willst du mich hacken oder so?");
+}
+
+async function getServerPicture(msg) {
+    let serverPicAttachment = msg.attachments.at(0);
+    if(serverPicAttachment.contentType === "image/png" || serverPicAttachment.contentType === "image/gif") {
+        userAttachments.find(entry => entry.username === msg.author.username).attachments.serverPic = serverPicAttachment.url;
+        return;
+    }
+    await msg.channel.send("Das war weder ein Bild noch ein Gifs...willst du mich hacken oder so?");
+}
+
+client.on("messageCreate", async msg => {
+    if(msg.author.bot) return;
+    if(msg.content === "stop") {
+        await channel.send("Ok, dann halt nicht :(");
+        return;
+    }
+
+    if(msg.attachments.size < 1) {
+        await msg.channel.send("Bruder, wo ist das Bild? Fang nochmal von vorne an...");
+        return;
+    }
+
+    userAttachments.push({"username": msg.author.username, attachments: {}});
+    if(state === -1) {
+        await getBanner(msg);
+        state = 1;
+
+        await msg.channel.send("Als nächstes schicke bitte das Profilbild!");
+        return;
+    } else if(state === 1) {
+        getServerPicture(msg);
+    }
+
+    const userContent = userAttachments.find(entry => entry.username === msg.author.username);
+    // check if banner and profile pic are given
+    if(userContent.attachments.banner && userContent.attachments.serverPic) {
+        await msg.channel.send("Irgendwas ist mit den Anhängen falsch gelaufen...");
+        return;
+    }
+
+    const contestChannel = client.channels.cache.get(config.send_to_channel);
+    await contestChannel.send({
+        files: [userContent.attachments.banner, userContent.attachments.serverPic]
+    });
+    
 });
 
 client.login(config.token).then(() => {
