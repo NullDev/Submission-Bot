29fe529c92af41e4bcaa99c82c14fb1ea355e1a9
NullDev (Shadow) <chris@nulldev.org>
2022-11-03 02:03:03 +0100

    Welp. refactored I guess?

--- src/app.js
+++ src/app.js
@@ -1,96 +1,58 @@
 "use strict";
 
-const { Client, Events, Intents, Message } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES] });
+// Dependencies
+const { Client, Intents, MessageActionRow, MessageButton } = require("discord.js");
+
+// Utils
+const startInteraction = require("./modules/startInteraction");
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES],
+});
 
 const config = require("../config.json");
-
-const userAttachments = [];
-let state;
 
 client.on("ready", async() => {
     console.log(`Logged in as ${client.user?.tag}!`);
     client.user?.setActivity(config.activity, { type: "PLAYING" });
 
-    // register new slash command
     await client.guilds.cache.get(config.guild)?.commands.create({
         name: "submit",
         description: "Bilder für den Contest submitten",
     });
+
+    await client.guilds.cache.get(config.guild)?.commands.create({
+        name: "createbutton",
+        description: "Erstellt einen Button zum Submitten",
+    });
 });
 
-// handle submit command
 client.on("interactionCreate", async interaction => {
-    if (!interaction.isCommand()) return;
+    if (interaction.isCommand()){
+        if (interaction.commandName === "createbutton"){
+            if (interaction.user.id !== config.admin) return;
 
-    if (interaction.commandName === "submit"){
-        await interaction.reply({ content: "Check deine PN's!", ephemeral: true });
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId("submit")
+                        .setLabel("Einreichen")
+                        .setStyle("PRIMARY"),
+                );
 
-        // send DM to user
-        const sent = await interaction.user.send({
-            content:
-`Hallo!
-Hier kannst du deine Bilder submitten.
-Wenn du es dir anders überlegst, schreibe einfach "stop"!
+            await interaction.channel?.send({
+                content: "Klicke auf den Button um eine Submission zu starten!\nFalls das nicht funktioniert, versuch `/submit`",
+                components: [row],
+            });
 
-Bitte poste das **BANNER**:`,
-        });
-    }
-    state = -1;
-});
-
-async function getAttachment(msg) {
-    let attachment = msg.attachments.at(0);
-    if(attachment.contentType === "image/png" || attachment.contentType === "image/gif") {
-        if(state === 1) {
-            userAttachments.find(entry => entry.username === msg.author.username).attachments.serverPic = attachment.url;
-        } else if(state === -1) {
-            userAttachments.find(entry => entry.username === msg.author.username).attachments.banner = attachment.url;
-            await msg.channel.send("Als nächstes schicke bitte das Profilbild!");
-            state = 1;
+            await interaction.reply({ content: "Button erstellt!", ephemeral: true });
         }
-        return;
+
+        if (interaction.commandName === "submit") startInteraction(interaction, client, config);
     }
-    await msg.channel.send("Das war weder ein Bild noch ein Gifs...willst du mich hacken oder so?");
-}
-
-client.on("messageCreate", async msg => {
-    if(msg.author.bot) return;
-    if(msg.content === "stop") {
-        await channel.send("Ok, dann halt nicht :(");
-        return;
+    else if (interaction.isButton() && interaction.customId === "submit"){
+        startInteraction(interaction, client, config);
     }
-
-    if(msg.attachments.size < 1) {
-        await msg.channel.send("Bruder, wo ist das Bild? Fang nochmal von vorne an...");
-        return;
-    }
-
-    userAttachments.push({"username": msg.author.username, attachments: {}});
-    
-    getAttachment(msg);
-    if(state == -1) return;
-
-    const userContent = userAttachments.find(entry => entry.username === msg.author.username);
-    // check if banner and profile pic are given
-    if(!userContent.attachments.banner || !userContent.attachments.serverPic) {
-        await msg.channel.send("Irgendwas ist mit den Anhängen falsch gelaufen...");
-        return;
-    }
-
-    const contestChannel = client.channels.cache.get(config.send_to_channel);
-    await contestChannel.send({
-        files: [userContent.attachments.banner, userContent.attachments.serverPic]
-    });
-    
-    // add reaction
-    contestChannel.messages.fetch({limit: 1}).then(messages =>{
-        const lastMessage = messages.first();
-        lastMessage.react("⭐");
-    });
-    
 });
 
-client.login(config.token).then(() => {
-    console.log("Logged in!");
-});
+client.login(config.token).then(() => console.log("Logged in!"));
